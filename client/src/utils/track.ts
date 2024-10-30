68b90546cc6fd4f46510102c88a6e1c2a83a380a
Joe Boylson <joeboylson@gmail.com>
2024-10-29 20:38:14 -0400

    use range 0-100 for speechiness parameter;

--- client/src/utils/track.ts
+++ client/src/utils/track.ts
@@ -1,6 +1,8 @@
 import { propertyDescriptions } from "../constants";
 import { PropertyDataKey } from "../enums";
 
+const { POPULARITY, SPEECHINESS } = PropertyDataKey;
+
 export function getDefaultPropertyData() {
   const value: [number, number] = [20, 80];
   const active = false;
@@ -9,7 +11,8 @@ export function getDefaultPropertyData() {
     const description = propertyDescriptions[key] as string;
 
     const valueModifier = ([a, b]: [number, number]) => {
-      if (key === PropertyDataKey.POPULARITY) return [a, b] as [number, number];
+      if ([POPULARITY, SPEECHINESS].includes(key))
+        return [a, b] as [number, number];
       return [a / 100, b / 100] as [number, number];
     };
 
